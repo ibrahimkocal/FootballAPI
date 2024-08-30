6cb1093452ea50fc9bbe32d265a385966022cb30
İbrahim Koçal <ibrahiimkocal@gmail.com>
2024-08-30 20:57:35 +0300

    düzenlemeler

--- js/superlig.js
+++ js/superlig.js
@@ -1,2 +1,14 @@
-let teamsInfo = document.querySelector("#teamsInfo");
+let teamInfo = document.querySelector("#teamInfo");
+let teamImage = document.querySelector("#teamImage");
+let teamName = document.querySelector("#teamName");
+
+fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=Turkish%20Super%20Lig`)
+  .then(response => response.json())
+  .then(data => {
+    data.teams.forEach(team => {
+      console.log(team.strTeam);
+      
+    });
+  })
+  .catch(error => console.error('Error:', error));
 
