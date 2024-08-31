51a600df067c2911716959a4d05444aeccd4400a
İbrahim Koçal <ibrahiimkocal@gmail.com>
2024-08-31 17:54:40 +0300

    s

--- js/superlig.js
+++ js/superlig.js
@@ -7,21 +7,29 @@ let leaguePoster = document.querySelector("#leaguePoster");
 fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_leagues.php?c=Turkiye`)
   .then(response => response.json())
   .then(data => {
-    const leagues = data.countries;
+    let leagues = data.countries;
+    let league_Name = leagues[0].strLeague;
+    let league_Kurulus = leagues[0].intFormedYear;
+    let league_Country = leagues[0].strCountry;
+    let league_Tv = leagues[0].strTvRights;
+    let league_Logo = leagues[0].strBadge;
+    let league_Poster = leagues[0].strPoster;
+
+    leagueName_Title.textContent = `${league_Name}`
     about.innerHTML = `<h5>Kuruluş</h5>
-              <p>${leagues[0].intFormedYear}</p>
+              <p>${league_Kurulus}</p>
               <br>
               <h5>Ülke</h5>
-              <p>${leagues[0].strCountry}</p>
+              <p>${league_Country}</p>
               <br>
               <h5>TV</h5>
-              <p>${leagues[0].strTvRights}</p>`
+              <p>${league_Tv}</p>`
     leagueName.innerHTML = `<h5>Lig Adı</h5>
-              <a id="leagueName" href="">${leagues[0].strLeague}</a>`              
+              <a id="leagueName" href="">${league_Name}</a>`              
     leagueLogo.innerHTML = `<h5>Logo</h5>
-              <img src="${leagues[0].strBadge}" width="190">`    
+              <img src="${league_Logo}" width="190">`    
     leaguePoster.innerHTML = `<h5>Poster</h5>
-              <img src="${leagues[0].strPoster}" width="220">`                     
+              <img src="${league_Poster}" width="220">`                     
   })
   .catch(error => console.error('API isteği sırasında bir hata oluştu:', error)); 
 
